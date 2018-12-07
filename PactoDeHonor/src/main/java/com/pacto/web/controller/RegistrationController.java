ff6a3c48c6ae422059845e1eeb6aaa028c3ad200
Cristian Garzon <cristiangarzonbernal@gmail.com>
2018-12-06 22:18:11 -0500

    Update RegistrationController.java

--- PactoDeHonor/src/main/java/com/pacto/web/controller/RegistrationController.java
+++ PactoDeHonor/src/main/java/com/pacto/web/controller/RegistrationController.java
@@ -2,15 +2,15 @@
 package com.pacto.web.controller;
 
 import javax.validation.Valid;
-
-import com.pacto.web.model.User;
-import com.pacto.web.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import com.pacto.web.model.User;
+import com.pacto.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 
 
 @Controller
@@ -34,18 +34,13 @@ public class RegistrationController {
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) throws InterruptedException {
         ModelAndView mv = new ModelAndView();
         User userExists = userService.findUserByEmail(user.getEmail());
-        if (userExists != null) {
-            bindingResult.rejectValue("email", "error.user", "Este email ya esta en uso");
-        }
         if (bindingResult.hasErrors()) {
             mv.setViewName("registration");
         } else {
             userService.saveUser(user);
-
             mv.addObject("user", new User());
            // mv.wait();
             mv.setViewName("login");
-            mv.addObject("successMessage", "has sido registrado");
         }
         return mv;
     }}
